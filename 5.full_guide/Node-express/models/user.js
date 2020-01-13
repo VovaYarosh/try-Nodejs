9327734967a2f0a9e7f6353bb69648cd7882df69
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-13 19:57:14 +0200

    add new user

---
+++ 5.full_guide/Node-express/models/user.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = new({
+    email:{
+        type: String,
+        require: true
+    },
+    name:{
+        type:String,
+        required: true
+    },
+    cart:{
+        items:[
+            {
+                count:{
+                    type: Number,
+                    required: true,
+                    default:1
+                },
+                courseId:{
+                    type: Schema.Types.ObjectId,
+                    ref:'Course',
+                    required: true
+
+                }
+            }
+        ]
+    }
+})
+
+module.exports = model('User', userSchema)
