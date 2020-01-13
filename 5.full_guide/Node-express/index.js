9f9677da15f119ac94b7d1eb5190cc2f668247f3
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-13 19:48:26 +0200

    user model

--- 5.full_guide/Node-express/index.js
+++ 5.full_guide/Node-express/index.js
@@ -31,8 +31,11 @@ const PORT = process.env.PORT || 3000
 
 async function start(){
     try{
-        const url = `mongodb+srv://VovaYarosh:qwerty1234@cluster0-9chaf.mongodb.net/test?retryWrites=true&w=majority`
-        await mongoose.connect(url,{useNewUrlParser: true})
+        const url = `mongodb+srv://VovaYarosh:qwerty1234@cluster0-9chaf.mongodb.net/shop`
+        await mongoose.connect(url,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
 
         app.listen(PORT,()=>{ 
             console.log(`server is running on port ${PORT}`)
