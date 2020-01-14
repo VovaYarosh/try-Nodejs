5dbd36c62aad105ffd267291670fff8321ddd31d
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-14 21:47:47 +0200

    orders output

--- 5.full_guide/Node-express/models/user.js
+++ 5.full_guide/Node-express/models/user.js
@@ -60,9 +60,14 @@ userSchema.methods.removeFromCart = function(id){
     }
 
     this.cart = {items}
-
     return this.save()
 }
+
+    userSchema.methods.clearCart = function(){
+        this.cart = {items: []}
+        return this.save()
+    }
+
 
 
 module.exports = model('User', userSchema)
