971d87fafe31590c698cc64fc80cbaf0da51eadb
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-15 23:26:38 +0200

    CSRF-defense

--- 5.full_guide/Node-express/routes/auth.js
+++ 5.full_guide/Node-express/routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router()
 
@@ -16,15 +17,33 @@ router.get('/logout', async (req,res) => {
 })
 
 router.post('/login', async (req,res) =>{
-    const user = await User.findById('5e1cd14aa56f552058c54498')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-        if(err){
-            throw err
-        }
-        res.redirect('/')
-    })
+    try{
+        const {email,password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if(candidate){
+            const areSame = await bcrypt.compare(password, candidate.password)
+            if(areSame){
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+                req.session.save(err => {
+                    if(err){
+                    throw err
+                    }
+                    res.redirect('/')
+                })
+
+                }else{
+                res.redirect('/auth/login#login')
+                }
+            }else{
+            res.redirect('/auth/login#login')
+            }
+    }catch(e){
+        console.log(e)
+    }
+
 })
 
 router.post('/register', async (req,res)=>{
@@ -36,8 +55,9 @@ router.post('/register', async (req,res)=>{
         if(candidate){
             res.redirect('/auth/login#register')
         }else{
+            const hashPassword = await bcrypt.hash(password, 10)
             const user = new User({
-                email,name,password,cart:{items: []}
+                email,name,password: hashPassword,cart:{items: []}
             })
             await user.save()
             res.redirect('/auth/login#login')
