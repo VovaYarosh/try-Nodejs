40ccbd5e2283ed5e7fd05831d3beb849035d5ac3
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-16 18:52:50 +0200

    email sending

--- 5.full_guide/Node-express/keys/index.js
+++ 5.full_guide/Node-express/keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI:`mongodb+srv://VovaYarosh:qwerty1234@cluster0-9chaf.mongodb.net/shop`,
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API:'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'
 }
