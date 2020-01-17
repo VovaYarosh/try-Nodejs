64c312d2e07b0cd68de73203b317df96917eed0e
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-17 21:26:04 +0200

    course validate

--- 5.full_guide/Node-express/utils/validators.js
+++ 5.full_guide/Node-express/utils/validators.js
@@ -2,7 +2,9 @@ const {body} = require('express-validator')
 const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Enter the correct password').custom(async (value, {req})=>{
+    body('email')
+    .isEmail().withMessage('Enter the correct password')
+    .custom(async (value, {req})=>{
         try{
             const user = await User.findOne({email: value})
             if (user){
@@ -11,13 +13,19 @@ exports.registerValidators = [
         }catch(e){
             console.log(e)
         }
-    }),
-    body('password').isLength({min: 6,max: 56}).isAlphanumeric(),
+    })
+    .normalizeEmail(),
+    body('password')
+    .isLength({min: 6,max: 56})
+    .isAlphanumeric()
+    .trim(),
     body('confirm').custom((value, {req})=>{
         if(value !== req.body.password){
             throw new Error('passwords must match')
         }
         return true
-    }),
+    })
+    .trim(),
     body('name').isLength({min:2}).withMessage('The name must be at least two symbols long')
+    .trim()
 ]
