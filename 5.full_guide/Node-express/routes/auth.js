4e4ccb1bf7089045e18e3b52531f571d79a3bde3
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-17 12:29:27 +0200

    refactoring of the law access

--- 5.full_guide/Node-express/routes/auth.js
+++ 5.full_guide/Node-express/routes/auth.js
@@ -147,4 +147,27 @@ router.post('/reset',  (req,res)=>{
     }
 })
 
+router.post('/password', async(req,res)=>{
+    try{
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken:req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if(user){
+            user.password = await bcrypt.hash(req.body.password, 10)
+            user.resetToken = undefined
+            user.resetTokenExp = undefined
+            await user.save()
+            res.redirect('/auth/login')
+        }else{
+            req.flash('loginError','Lifecycle of token is end')
+            res.redirect('/auth/login')
+        }
+    }catch(e){
+        console.log(e)
+    }
+})
+
 module.exports = router
