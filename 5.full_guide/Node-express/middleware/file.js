c4db897eb538231765f29bad5fbf587062fa1a4f
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-18 17:20:20 +0200

    key defense

--- 5.full_guide/Node-express/middleware/file.js
+++ 5.full_guide/Node-express/middleware/file.js
@@ -1,11 +1,11 @@
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination(req,file,cb){
+    destination(req, file, cb){
         cb(null,'images')
     },
     filename(req,file,cb){
-        cb(null,new Date().toISOString() + '-' + file.originalname)
+        cb(null,new Date().toISOString().replace(/:/g,'-') + '-' + file.originalname)
     }
 })
 
