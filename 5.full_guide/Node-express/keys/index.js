b9717d4ae86fdbc59fddc6cbb3a891846f5467ef
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-18 17:34:55 +0200

    adding headers

--- 5.full_guide/Node-express/keys/index.js
+++ 5.full_guide/Node-express/keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI:`mongodb+srv://VovaYarosh:qwerty1234@cluster0-9chaf.mongodb.net/shop`,
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY:'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',
-    EMAIL_FROM:'nodejs@email.ua',
-    BASE_URL: 'http://localhost:3000'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys.prod')
+}else{
+    module.exports = require('./keys.dev')
 }
---
+++ 5.full_guide/Node-express/keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI:`mongodb+srv://VovaYarosh:qwerty1234@cluster0-9chaf.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY:'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',
+    EMAIL_FROM:'nodejs@email.ua',
+    BASE_URL: 'http://localhost:3000'
+}
