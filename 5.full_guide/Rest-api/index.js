9a3172ed0ddf589c227a2cd7b591c6315b4cf5e9
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-19 08:30:43 +0200

    creating a model

--- 5.full_guide/Rest-api/index.js
+++ 5.full_guide/Rest-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 //підключаєм роути до головного файлу
 const todoRoutes = require('./routes/todo')
 const app = express()
@@ -17,4 +18,16 @@ app.use((req,res,next)=>{
     res.sendFile('/index.html')
 })
 
-app.listen(PORT)
+
+//запуск бази і сервера
+async function start(){
+    try{
+        await sequelize.sync()
+        app.listen(PORT)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start()
+
