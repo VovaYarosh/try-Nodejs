8d66708eb3b5e997d01ecb600310cb70412cfca2
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-22 22:46:04 +0200

    graphql how it works

---
+++ 5.full_guide/GraphQL/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const graphqlHTTP = require('express-graphql')
+const sequelize = require('./utils/database')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
+const app = express()
+const PORT = process.env.port || 3000
+
+//використовуєм модуль path щоб вказати шлях до статичної папки паблік
+app.use(express.static(path.join(__dirname, "public")))
+//щоб привести буфер до обєкту в пост запиті використовуєм мідлвер
+app.use(express.json())
+
+app.use(graphqlHTTP({
+    schema:schema,
+    rootValue:resolver
+}))
+//даний мідлвер буде завжди іти перед запуском сервера
+//потрібно на кожний запит повертати один файл для цього викликається метод res.sendFile
+app.use((req,res,next)=>{
+    res.sendFile('/index.html')
+})
+
+
+//запуск бази і сервера
+//параметр {force:true} дозволяє видаляти таблиці з бази,видаливши з коду
+//такий параметр краще забирати піся першого дроптейбла
+async function start(){
+    try{
+        await sequelize.sync()
+        app.listen(PORT)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start()
+
