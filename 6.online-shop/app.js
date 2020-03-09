658458309c593dcc50725114ba82a02d0b27c6e5
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-03-09 13:53:05 +0200

    will render goods categ

--- 6.online-shop/app.js
+++ 6.online-shop/app.js
@@ -1,13 +1,36 @@
-let express = require('express')
+let express = require('express');
+let mysql = require('mysql');
 let app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-app.listen(3000,function(){
-    console.log('Server is working')
+let con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'shop'
 })
 
-// app.get('/',function(req,res){
-// })
+app.listen(3000, function () {
+    console.log('node express work on 3000');
+});
+
+app.get('/', function(req,res){
+    con.query(
+        'SELECT * FROM goods',
+        function(error,result){
+            if(error) throw error;
+            let goods = {}
+            for(let i = 0;i <result.length;i++){
+                goods[result[i]['id']] = result[i];
+            }
+            res.render('main',{
+                goods: JSON.parse(JSON.stringify(goods))
+            })
+        }
+    );
+})
+
+
