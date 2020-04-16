bb0b4c94f348dc5e7f94b97759a5c0be1d3cfcbc
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-04-16 17:24:28 +0300

    connected to the database with mongoose

--- 8.dev_camper_api/server.js
+++ 8.dev_camper_api/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db')
+
 
 //load env vars
 dotenv.config({path: './config/config.env'});
 
+//connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
-
-
 
 // dev logging middleware
 if(process.env.NODE_ENV == 'development'){
@@ -22,4 +26,14 @@ app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    );
+
+    //handle unhandled promise rejections
+    process.on('unhandledRejection',(err,promise) => {
+        console.log(`error: ${err.message}`);
+        //close server & exit process
+        server.close(() => process.exit(1))
+    })
