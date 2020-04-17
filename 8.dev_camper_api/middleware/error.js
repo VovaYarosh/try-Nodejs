5837a1d51e4fe8f9c6e8766ed1a970e903480a75
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-04-17 17:39:47 +0300

    mongoose error handling 2

--- 8.dev_camper_api/middleware/error.js
+++ 8.dev_camper_api/middleware/error.js
@@ -5,13 +5,24 @@ const errorHandler = (err,req,res,next) => {
 
     error.message = err.message;
     //log to console fro dev
-    console.log(err.stack.red)
+    console.log(err);
 
     //mongoose bad  objectId
     if(err.name === 'CastError'){
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
 
+    //mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    //mongoose validation error
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorResponse(message,400);
     }
 
     res.status(error.statusCode || 500).json({
