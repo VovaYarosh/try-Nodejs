99afc7259682ff35b7cfc3b83992c97bfeaa7901
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-20 16:49:01 +0300

    review model GET reviews

--- 8.dev_camper_api/server.js
+++ 8.dev_camper_api/server.js
@@ -21,6 +21,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -45,8 +46,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/auth', users);
-
+app.use('/api/v1/user', users);
+app.use('/api/v1/reviews', reviews);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3500;
