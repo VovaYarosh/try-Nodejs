ca64b6ee7634b54e652de71cc6e3e4352e0810cd
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-20 17:03:59 +0300

    GET single review & update seeder

--- 8.dev_camper_api/seeder.js
+++ 8.dev_camper_api/seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -27,6 +28,8 @@ const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8'));
 
 //import into DB
 const importData = async () => {
@@ -48,6 +51,7 @@ const deleteData = async () => {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
         await User.deleteMany();
+        await Review.deleteMany();
 
         console.log('data destroyed...'.red.inverse);
         process.exit()
