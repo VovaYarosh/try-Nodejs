4c399ab40909df2e874003635b08a0a84556f1ae
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-21 19:07:45 +0300

    xss protection & security headers

--- 8.dev_camper_api/server.js
+++ 8.dev_camper_api/server.js
@@ -6,8 +6,10 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
-const errorHandler = require('./middleware/error');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 
@@ -42,6 +44,12 @@ app.use(fileupload());
 
 //sanitize data
 app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+//prevent xss attacks
+app.use(xss())
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
