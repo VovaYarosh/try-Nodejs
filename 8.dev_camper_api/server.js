d93712cfcc56bfba8b29d510a213d47d4cd1d4c2
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-21 19:21:18 +0300

    rate limiting, hpp & cors

--- 8.dev_camper_api/server.js
+++ 8.dev_camper_api/server.js
@@ -8,7 +8,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
-
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -49,7 +51,21 @@ app.use(mongoSanitize());
 app.use(helmet());
 
 //prevent xss attacks
-app.use(xss())
+app.use(xss());
+
+//rate limiting
+const limited = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 minutes
+    max: 100
+});
+
+app.use(limited);
+
+//prevent http param pollution
+app.use(hpp());
+
+//enable Cors
+app.use(cors());
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
