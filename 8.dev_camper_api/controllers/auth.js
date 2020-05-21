9332cf4957ba1fc58eedac505ddf44971aab1695
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-21 18:44:26 +0300

    logout to clear token cookie

--- 8.dev_camper_api/controllers/auth.js
+++ 8.dev_camper_api/controllers/auth.js
@@ -51,8 +51,24 @@ exports.login = asyncHandler(async (req,res,next) => {
     sendTokenResponse(user, 200, res);
 });
 
+//@ desc    log user user /clear cookie
+//@route    GET /api/v1/auth/me
+//@access   private
 
-//@ desc    login user
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none',{
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+})
+
+
+//@ desc    get xurrent logged user user
 //@route    post /api/v1/auth/lme
 //@access   private
 
